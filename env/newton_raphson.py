87caf44423a7c46d0fe5fabc7e6168f39a16bd50
Jose Silva <josemarchan2603@gmail.com>
2023-11-11 19:49:11 -0400

    newton_raphson terminado

--- env/newton_raphson.py
+++ env/newton_raphson.py
@@ -1,16 +1,60 @@
-##Funcion para el metodo de newton-raphson
-def derivada(funcion, x, h):
-    #Esta funcion calcula la derivada de una funcion en un punto x
-    #usando el método de las diferencias finitas, h es el tamaño del paso
-    return (funcion(x + h) - funcion(x)) / h
+import math as ma
+
+def first_derivate(funcion, x, h=0.02): ##Calcular primera derivada
+    primera_derivada = (funcion(x + h) - funcion(x)) / h   
+    return primera_derivada
+
+def second_derivate(funcion, x, h=0.02): ##Calcular segunda derivada
+    segunda_derivada = (funcion(x + 2 * h) - 2 * funcion(x + h) + funcion(x)) / (h ** 2)
+    return segunda_derivada
 
 def function(x):
-    return x**2 + 2*x - 3
+    return ma.exp(x) - 3*x**2   ##Defino la funcion
 
-def calcular_derivadas(funcion, x, h=0.001):
-    primera_derivada = (funcion(x + h) - funcion(x)) / h
-    segunda_derivada = (funcion(x + 2 * h) - 2 * funcion(x + h) + funcion(x)) / (h ** 2)
 
-    return primera_derivada, segunda_derivada
+##Funcion para verificar si la funcion converge o no
+def verificarConvergencia(funcion, primera_derivada, segunda_derivada) -> float:  ##Recibe como parametro la funcion, la primera y segunda derivada
+    a = 1
+    b = 2
+    x0 = a / b
 
-print(calcular_derivadas(function, 1))
+    converge = funcion(x0) * segunda_derivada / pow(primera_derivada, 2)
+
+    if (abs(converge) < 1): ##Si el resultado es menor que 1 es porque converge, de lo contrario hay que ir a la formula iterativa
+        print(f"La funcion convergera en el punto {x0}")
+    else:
+        print("El teorema de Newton-Raphson no decide")
+    
+    return abs(converge)
+
+print(verificarConvergencia(function, first_derivate(function,0.5), second_derivate(function,0.5)))
+
+# ##Funcion para el metodo de newton-raphson
+def newton_raphson(x0, margen_error, max_iteraciones):
+    x0 = 0.5
+    iteracion = 0
+    error_relativo = 1
+    x = x0
+
+    while error_relativo > margen_error and iteracion < max_iteraciones:
+        x_nuevo = x - function(x) / first_derivate(function,x)
+        error_relativo = abs(x_nuevo - x)
+        x = x_nuevo
+        iteracion += 1
+
+    # Imprimir el resultado
+    if iteracion == max_iteraciones:
+        print("El método no converge después de", max_iteraciones, "iteraciones.")
+    else:
+        print("La raíz es:", x, "después de", iteracion, "iteraciones.")
+        return x
+
+if __name__ == '__main__':
+    # Definir el punto inicial, la tolerancia y el número máximo de iteraciones
+    x0 = 0.5
+    margen_error = 0.002
+    max_iteraciones = 100
+    
+    resultado = newton_raphson(x0, margen_error, max_iteraciones)
+    comprobacion = function(resultado)
+    print("\nEl resultado de la comprobacion es: ", abs(comprobacion))
